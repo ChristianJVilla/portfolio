42b741f84d00137c5723286e1c30adb030c4eb6b
christianvilla <christianj.villa@gmail.com>
2019-09-03 10:18:58 -0700

    removes phone number

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,7 +6,6 @@ const Contact = () => {
         <h3 className="about">Contact me</h3>
             <ul class="contact-li">
                 <li><a href="mailto:christianj.villa@gmail.com?subject=Hi there!" ><i class="fas fa-envelope"></i> christianj.villa@gmail.com</a></li>
-                <li><a href="tel:9259513111" ><i class="fas fa-phone"></i> 925.951.3111</a></li>
                 <li><a href="https://www.linkedin.com/in/christianjvilla"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
                 <li><a href="https://git.generalassemb.ly/christianvilla"><i class="fab fa-github-square"></i> github</a></li>
             </ul>
